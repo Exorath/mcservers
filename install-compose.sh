570231684f99b2e4d572062ffb684bb45f220d3a
Toon Sevrin <twan123@live.be>
2016-06-28 17:28:10 +0200

    Create install-compose.sh

---
+++ install-compose.sh
@@ -0,0 +1,4 @@
+mkdir /opt/
+mkdir /opt/bin
+curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /opt/bin/docker-compose
+chmod +x /opt/bin/docker-compose
